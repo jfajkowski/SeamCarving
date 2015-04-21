aada45e71f60a0be9e9918cd86c3decb0a96dfe3
jakubfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2015-04-21 22:10:58 +0200

    Create energy_demo.h

---
+++ energy_demo.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+int showEnergyDemo(Mat src);
+void energy(int, void*);
