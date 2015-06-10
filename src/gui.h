ab808dd54b656bee9d7f0f9003518d7c4394e46f
jakubfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2015-06-10 23:44:45 +0200

    Update gui.h

--- src/gui.h
+++ src/gui.h
@@ -2,6 +2,7 @@
 #define GUI_H
 
 #include <QtWidgets/QMainWindow>
+#include <string>
 #include "ui_gui.h"
 #include "image.h"
 #include "energy_demo.h"
@@ -13,23 +14,25 @@ class gui : public QMainWindow
 public:
 	gui(QWidget *parent = 0);
 	~gui();
+	void errorDialog(std::string s);
+	void setMaxNumbersOfSeams();
 
 public slots:
 	void clickedLoad();
-	void clickedAddV();
-	void clickedDelV();
-	void clickedAddH();
-	void clickedDelH();
+	void clickedAdd();
+	void clickedDel();
 	void clickedEnergy();
 	void clickedDemo();
-	void changedNumberOfSeams(int k);
+	void changedNumberOfVerticalSeams(int k);
+	void changedNumberOfHorizontalSeams(int k);
 	void setSobel() { edge_type = 0; }
 	void setScharr() { edge_type = 1; }
 	void setCanny() { edge_type = 2; }
 
 private:
 	Ui::guiClass ui;
-	int number_of_seams;
+	int number_of_vertical_seams;
+	int number_of_horizontal_seams;
 	Image i;
 	Mat src;
 };
